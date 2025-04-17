6d40a0d5fae88c05d35391d353c2f9f114fc5329
colorcube00 <a31289584@gmail.com>
2025-04-17 20:41:26 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import HomePage from './pages/home/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+// import GalleryPage from '/pages/gallery/GalleryPage';
+// import BlogPage from '/pages/Blog/BolgPage';
+// import AboutMePage from './pages/aboutMe/AboutMePage';
+// import MyStoryPage from '.pages/MyStory/MyStoryPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/about-me" element={<AboutMePage />} />
+            <Route path="/gallery" element={<GalleryPage />} />
+            <Route path="/blog" element={<BlogPage />} />
+            <Route path="/my-story" element={<MyStoryPage/>} /> */}
+          </Routes>
+          <footer className="app-footer">
+            <Footer />
+          </footer>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/hooks/home/useHome.js
@@ -0,0 +1,86 @@
+import {useState, useMemo, useEffect} from 'react';
+import Old_brother from '../../assets/images/woowakgood.jpg';
+
+function useHome() {
+    const allProjects = useMemo(() => [
+        {
+            id: 1,
+            title: "Weather App",
+            description: "React 기반 날씨 앱 프로젝트",
+            image:Old_brother,
+            category:"Web",
+            link: "https://example.com/Weather-app"
+        },
+        {
+            id: 2,
+            title: "Todo List",
+            description: "리엑트 상태콴리를 배운 TODO 리스트",
+            image: Old_brother,
+            category:"Web",
+            link: "https://example.com/Weather-app"
+        },
+        {
+            id: 3,
+            title: "Portfolio site",
+            description: "나의 작업을 보여주는 포트폴리오",
+            image: Old_brother,
+            category:"Design",
+            link: "https://example.com/Weather-app"
+        },
+        {
+            id: 4,
+            title: "Moblie UI Design",
+            description: "나의 작업을 보여주는 포트폴리오",
+            image:Old_brother,
+            category:"Web",
+            link: "https://example.com/Weather-app"
+        }
+    ], []);
+
+    const [projects, setProjects] = useState([]);
+    const [activeCategory, setActiveCategory] = useState('All');
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setProjects(allProjects);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [allProjects]);
+
+    useEffect(() => {
+        if(activeCategory === 'All') {
+            setProjects(allProjects);
+        } else {
+            const filtered = allProjects.filter(projects => projects.category === activeCategory);
+            setProjects(filtered);
+        }
+    }, [activeCategory, allProjects]);
+
+        useEffect(() => {
+            if(isDarkMode) {
+                document.body.classList.add('dark-mode');
+            }else {
+                document.body.classList.remove('dark-mode');
+            }
+        }, [isDarkMode]);
+
+        const handleCategoryChange = (category) => {
+            setActiveCategory(category);
+        };
+
+        const toggleDarkMode = () => {
+            setIsDarkMode(!isDarkMode);
+        };
+
+        return {
+            projects,
+            activeCategory,
+            handleCategoryChange,
+            isDarkMode,
+            toggleDarkMode
+        };
+}
+
+export default useHome;
