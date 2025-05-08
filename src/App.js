d551bff07f67d89dc76b9b5136f9d3c51981f45e
colorcube00 <a31289584@gmail.com>
2025-05-08 20:27:47 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './pages/home/HomePage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 // import GalleryPage from '/pages/gallery/GalleryPage';
@@ -17,7 +17,7 @@ function App() {
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<navigator to="/home"/>} />
+            <Route path="/" element={<Navigate to="/home"/>} />
             <Route path="/" element={<HomePage />} />
             <Route path="/about-me" element={<AboutMePage />} />
             {/* <Route path="/gallery" element={<GalleryPage />} />
